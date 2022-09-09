366c8b7b82db472855c45f89fff554fe41fc7e1d
Jakub Furtak <kuba.furtak.pl@gmail.com>
2022-09-09 16:51:29 +0200

    added ability to change alarm time when clicked on a alarm

--- app/src/main/java/jf/clock/ClockFragment.java
+++ app/src/main/java/jf/clock/ClockFragment.java
@@ -5,16 +5,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.navigation.NavBackStackEntry;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ClockFragment extends Fragment {
@@ -22,6 +34,7 @@ public class ClockFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
     private AlarmAdapter mAdapter;
+    private List<Alarm> mAlarmList;
     private Alarm mAlarm;
 
     @Override
@@ -35,6 +48,15 @@ public class ClockFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.clock_fragment, container, false);
 
+        getChildFragmentManager().setFragmentResultListener("requestKey",
+                this, (requestKey, result) -> {
+                    if (requestKey.equals("requestKey")){
+                        int pos = result.getInt("position");
+                        mAlarmList.get(pos).setAlarmTime((Date) result.getSerializable("date"));
+                        mAdapter.notifyItemChanged(pos);
+                    }
+                });
+
         return v;
     }
 
@@ -43,26 +65,72 @@ public class ClockFragment extends Fragment {
 
         mRecyclerView = view.findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
-        List<Alarm> alarms = new ArrayList<>();
+        mAlarmList = new ArrayList<>();
         for (int i = 0; i < 5;i++){
             Calendar calendar = Calendar.getInstance();
             calendar.set(Calendar.HOUR_OF_DAY,i);
             calendar.set(Calendar.MINUTE, i);
             Alarm alarm = new Alarm();
             alarm.setAlarmTime(calendar.getTime());
-            alarms.add(alarm);
+            mAlarmList.add(alarm);
         }
 
-        String[] yoyo = {
-                "06:30",
-                "09:00",
-                "04:20"
-        };
-
-
-        mAdapter = new AlarmAdapter(alarms);
+        mAdapter = new AlarmAdapter(mAlarmList);
         mRecyclerView.setAdapter(mAdapter);
 
+    }
+
+    public class AlarmAdapter extends RecyclerView.Adapter<AlarmAdapter.ViewHolder>{
+        private List<Alarm> mAlarms;
+
+        public AlarmAdapter(List<Alarm> alarms) {
+            mAlarms = alarms;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            private TextView mTextView;
+            private SwitchCompat mSwitch;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                itemView.setOnClickListener(this);
+                mTextView = itemView.findViewById(R.id.alarm_id);
+                mSwitch = itemView.findViewById(R.id.alarm_switch);
+
+                // do things with your elements in each row
+            }
+
+            public void bind(Alarm alarm){
+                mTextView.setText(alarm.getAlarmTime().toString());
+            }
+
+            @Override
+            public void onClick(View v) {
+                // TODO date is saved as String, change to Date if possible
+                Date date = mAlarms.get(getAdapterPosition()).getAlarmTime();
+
+                FragmentManager fm = getChildFragmentManager();
+                TimePicker dialog = TimePicker.newInstance(date, getAdapterPosition());
+                dialog.show(fm, "dialog");
+
+            }
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new AlarmAdapter.ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.alarm_row, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.bind(mAlarms.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mAlarms.size();
+        }
     }
 
 }
--- app/src/main/java/jf/clock/TimePicker.java
+++ app/src/main/java/jf/clock/TimePicker.java
@@ -1,9 +1,9 @@
 package jf.clock;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -12,16 +12,25 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
-import androidx.fragment.app.DialogFragment;
-import androidx.navigation.NavDirections;
 
 import java.util.Calendar;
 import java.util.Date;
+
 
 public class TimePicker extends AppCompatDialogFragment {
     private static final String TAG = "TimePicker";
 
     private android.widget.TimePicker mTimePicker;
+
+    public static TimePicker newInstance(Date date, int position) {
+
+        Bundle args = new Bundle();
+        args.putSerializable("date", date);
+        args.putInt("position", position);
+        TimePicker fragment = new TimePicker();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @NonNull
@@ -29,17 +38,35 @@ public class TimePicker extends AppCompatDialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         View v = LayoutInflater.from(requireContext()).inflate(R.layout.time_setter, null);
 
-        TimePickerArgs args = TimePickerArgs.fromBundle(getArguments());
+        Date date = (Date) getArguments().getSerializable("date");
+        int position = getArguments().getInt("position");
 
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(args.getDate());
+        calendar.setTime(date);
+
         mTimePicker = v.findViewById(R.id.time_picker);
+        mTimePicker.setIs24HourView(true);
         mTimePicker.setHour(calendar.get(Calendar.HOUR_OF_DAY));
         mTimePicker.setMinute(calendar.get(Calendar.MINUTE));
 
         return new AlertDialog.Builder(requireContext())
                 .setView(v)
                 .setTitle("yo")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Calendar calendar1 = Calendar.getInstance();
+                        calendar1.set(Calendar.HOUR_OF_DAY, mTimePicker.getHour());
+                        calendar1.set(Calendar.MINUTE, mTimePicker.getMinute());
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("date", calendar1.getTime());
+                        bundle.putInt("position", position);
+
+                        getParentFragmentManager().setFragmentResult("requestKey", bundle);
+
+                    }
+                })
                 .create();
     }
 }
